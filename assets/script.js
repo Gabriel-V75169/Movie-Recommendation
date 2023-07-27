2cbf8a2f567047734e56d46811f03520f1915718
Justin Bobrick <j.bobrick@outlook.com>
2023-07-26 21:39:15 -0700

    Added code to search by romance genre

--- assets/script.js
+++ assets/script.js
@@ -1,88 +1,155 @@
 // TMDb aka The Movie Database
-const apiKey = 'f406f93384ce6002da3472b812f47858';
-const searchInput = $('#movieTitle');
+const apiKey = "f406f93384ce6002da3472b812f47858";
+const searchInput = $("#movieTitle");
 const searchButton = $('button[type="submit"]');
-const resultsContainer = $('#results');
+const resultsContainer = $("#results");
 
 // event listener to wait for user to submit their movie input
-searchButton.on('click', function () {
+searchButton.on("click", function () {
   const movieTitle = searchInput.val();
   const searchURL = `https://api.themoviedb.org/3/search/movie?query=${movieTitle}&api_key=${apiKey}`;
-  
-// calls the searchMovie function using the movie database api search url from above
-  searchMovie(searchURL);
 
+  // calls the searchMovie function using the movie database api search url from above
+  searchMovie(searchURL);
 });
 
 // function to fetch data
 function searchMovie(url) {
   fetch(url)
-    .then(res => res.json())
-    .then(data => {
+    .then((res) => res.json())
+    .then((data) => {
       getMovieId(data.results);
     })
-    .catch(error => {
-      console.error('Error fetching data:', error);
+    .catch((error) => {
+      console.error("Error fetching data:", error);
     });
 }
 
-
 function getMovieId(data) {
-    if (data.length === 0) {
-      console.log('No movies found.');
-      return;
-    }
-//   selects the ID of the FIRST movie from the array of movies found by user input and
-    const MovieId = data[0].id;
-    console.log('Movie ID:', MovieId);
-// took me forever to get this to work because i was using & instead of ? before the api key, but "similar" is a feature of The Movie Database that shows similar movies based on movie id
-    const recommendURL = 'https://api.themoviedb.org/3/movie/'+ MovieId +'/similar?api_key='+ apiKey;
-    console.log(recommendURL);
-// calls the function to find movie recommendations based on user input
-    fetchRecommendations(recommendURL);
+  if (data.length === 0) {
+    console.log("No movies found.");
+    return;
+  }
+  //   selects the ID of the FIRST movie from the array of movies found by user input and
+  const MovieId = data[0].id;
+  console.log("Movie ID:", MovieId);
+  // took me forever to get this to work because i was using & instead of ? before the api key, but "similar" is a feature of The Movie Database that shows similar movies based on movie id
+  const recommendURL =
+    "https://api.themoviedb.org/3/movie/" +
+    MovieId +
+    "/similar?api_key=" +
+    apiKey;
+  console.log(recommendURL);
+  // calls the function to find movie recommendations based on user input
+  fetchRecommendations(recommendURL);
 }
 
 function fetchRecommendations(url) {
-    fetch(url)
-      .then(res => res.json())
-      .then(data => {
-        displayRecommendations(data.results);
-      })
-      .catch(error => {
-        console.error('Error fetching recommendations:', error);
-      });
-  }
-
-  function displayRecommendations(recommendations) {
-    resultsContainer.empty();
-    
-    const movieContainer = document.createElement('div');
-    movieContainer.classList.add('movie-container');
-  
-    recommendations.forEach(movie => {
-      const movieCard = document.createElement('div');
-      movieCard.classList.add('movie-card');
-  
-      // Create an element for the movie title
-      const movieTitle = document.createElement('h2');
-      movieTitle.textContent = movie.title;
-      movieCard.appendChild(movieTitle);
-  
-      // Create a container for the movie poster
-      const posterContainer = document.createElement('div');
-      posterContainer.classList.add('poster-container');
-  
-      // Create an image element for the movie poster
-      const poster = document.createElement('img');
-      poster.src = 'https://image.tmdb.org/t/p/w500' + movie.poster_path;
-      poster.alt = movie.title;
-      posterContainer.appendChild(poster);
-  
-      movieCard.appendChild(posterContainer);
-  
-      resultsContainer.append(movieCard);
-      movieContainer.appendChild(movieCard);
+  fetch(url)
+    .then((res) => res.json())
+    .then((data) => {
+      displayRecommendations(data.results);
+    })
+    .catch((error) => {
+      console.error("Error fetching recommendations:", error);
     });
-  
-    resultsContainer.append(movieContainer);
-  }
+}
+
+function displayRecommendations(recommendations) {
+  resultsContainer.empty();
+
+  const movieContainer = document.createElement("div");
+  movieContainer.classList.add("movie-container");
+
+  recommendations.forEach((movie) => {
+    const movieCard = document.createElement("div");
+    movieCard.classList.add("movie-card");
+
+    // Create an element for the movie title
+    const movieTitle = document.createElement("h2");
+    movieTitle.textContent = movie.title;
+    movieCard.appendChild(movieTitle);
+
+    // Create a container for the movie poster
+    const posterContainer = document.createElement("div");
+    posterContainer.classList.add("poster-container");
+
+    // Create an image element for the movie poster
+    const poster = document.createElement("img");
+    poster.src = "https://image.tmdb.org/t/p/w500" + movie.poster_path;
+    poster.alt = movie.title;
+    posterContainer.appendChild(poster);
+
+    movieCard.appendChild(posterContainer);
+
+    resultsContainer.append(movieCard);
+    movieContainer.appendChild(movieCard);
+  });
+
+  resultsContainer.append(movieContainer);
+}
+
+const genreApi = "a005c14e69msh0a4e62b61b8ee65p1f3f21jsn12df62b5ea9c";
+const actionBtn = $("#action");
+const adventureBtn = $("#adventure");
+const animationBtn = $("#animation");
+const biographyBtn = $("#biography");
+const comedyBtn = $("#comedy");
+const crimeBtn = $("#crime");
+const docBtn = $("#documentary");
+const dramaBtn = $("#drama");
+const familyBtn = $("#family");
+const fantasyBtn = $("#fantasy");
+const historyBtn = $("#history");
+const horrorBtn = $("#horror");
+const musicalBtn = $("#musical");
+const mysteryBtn = $("#mystery");
+const romanceBtn = document.getElementById('romance');
+const scifiBtn = $("#scifi");
+const sportsBtn = $("#sports");
+const thrillerBtn = $("#thriller");
+const westernBtn = $("#western");
+
+
+romanceBtn.addEventListener('click', searchRomanceMovies);
+
+// Function to search for romance movies
+function searchRomanceMovies() {
+  const genreKey = 'a005c14e69msh0a4e62b61b8ee65p1f3f21jsn12df62b5ea9c';
+
+  const apiUrl = `https://streaming-availability.p.rapidapi.com/v2/search/basic?country=us&services=netflix%2Cprime.buy%2Chulu.addon.hbo%2Cpeacock.free&output_language=en&show_type=movie&genre=10749&show_original_language=en&keyword=zombie`;
+
+  fetch(apiUrl, {
+    method: 'GET',
+    headers: {
+      'X-RapidAPI-Key': genreKey,
+      'X-RapidAPI-Host': 'streaming-availability.p.rapidapi.com'
+    }
+  })
+  .then(response => response.json())
+  .then(data => {
+    // Handle the API response
+    const moviesContainer = document.getElementById('genre-results');
+    moviesContainer.innerHTML = '';
+
+    data.result.forEach(movie => {
+      const movieElement = document.createElement('div');
+      movieElement.classList.add('movie');
+
+      const titleElement = document.createElement('h3');
+      titleElement.textContent = movie.title;
+      movieElement.appendChild(titleElement);
+
+      const posterElement = document.createElement('img');
+      posterElement.src = movie.posterURLs['185']; // Adjust the size as per your preference
+      posterElement.alt = movie.title;
+      movieElement.appendChild(posterElement);
+
+      moviesContainer.appendChild(movieElement);
+    });
+  })
+  .catch(error => {
+    // Handle any errors
+    console.error(error);
+  });
+}
