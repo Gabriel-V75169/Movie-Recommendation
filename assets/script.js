1292fc6ae959e953f1640331c1a2fd3be2c5cd0f
Hamza Lhamous <hamza.lhamous@gmail.com>
2023-07-31 21:28:30 -0700

    added language filter

--- assets/script.js
+++ assets/script.js
@@ -72,7 +72,8 @@ function displayRecommendations(recommendations) {
   movieContainer.classList.add("movie-container");
 
   // Filter out adult movies from recommendations
-  const filteredRecommendations = recommendations.filter((movie) => !movie.adult);
+  const filteredRecommendations = recommendations.filter(
+    (movie) => movie.adult === false && movie.original_language === "en");
 
   filteredRecommendations.forEach((movie) => {
     const movieCard = document.createElement("div");
